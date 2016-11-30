f3afdf80b321b045c8c9ffe68b3cd283687ee46b
Thomas Lehmann <thomas.lehmann.private@gmail.com>
2016-11-30 20:02:40 +0100

    Running queen algorithm with Perl.

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 ### Table Of Content
 [**Introduction**](#introduction)  
 [**Running with Python**](#running-with-python)  
+[**Running with Perl**](#running-with-perl)  
 
 ## Introduction
 Queen algorithm in different languages.
@@ -14,3 +15,15 @@ Queen algorithm in different languages.
 ```
 scripts/run_python3.5.sh
 ```
+
+You can check then `reports/Queen.py.log`
+for the results.
+
+## Running with Perl
+
+```
+scripts/run_perl520.sh
+```
+
+You can check then `reports/Queen.pl.log`
+for the results.
--- scripts/run_perl520.sh
+++ scripts/run_perl520.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 if [ -$# -eq 0 ]; then
-    docker run --rm=true -v $PWD:/docker centos:7.2.1511 /docker/scripts/run_python35.sh INIT
+    docker run --rm=true -v $PWD:/docker centos:7.2.1511 /docker/scripts/run_perl520.sh INIT
 else
     case $1 in
         INIT)
             # https://www.softwarecollections.org/en/scls/rhscl/rh-python35/
             yum install -y centos-release-scl yum-utils
             yum-config-manager --enable rhel-server-rhscl-7-rpms
-            yum install -y rh-python35
-            scl enable rh-python35 "/docker/scripts/run_python35.sh RUN"
+            yum install -y rh-perl520
+            scl enable rh-perl520 "/docker/scripts/run_perl520.sh RUN"
         ;;
         RUN)
-            OUT=/docker/reports/Queen.py.log
+            OUT=/docker/reports/Queen.pl.log
             rm -f "${OUT}"
             for n in $(seq 8 14); do
-                python /docker/src/Queen.py "${n}" | tee --append "${OUT}"
+                perl /docker/src/Queen.pl "${n}" | tee --append "${OUT}"
             done
         ;;
     esac
